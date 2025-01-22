97d8e2b83bf24c74c5224c808c47687708f3d412
LokeshKumar1779 <kumarlokesh57@gmail.com>
2025-01-22 16:29:57 +0530

    added/modified/deleted

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,6 +1,9 @@
 package pages;
 
 class LoginPage {
+
+     String username = "admin";
+     String pwd = "admin";
     public static void main(String [] args){
         System.out.println("Hello from LP");
     }
--- src/tests/HomeTest.java
+++
@@ -1,7 +0,0 @@
-package tests;
-
-class HomeTest {
-    public void method1(){
-        System.out.println("Hello from HomeTest");
-    }
-}
